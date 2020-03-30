32b073fc7d4f1fd08765f211e66b1fbbcf98ff8c
Respawn <markoff.g@gmail.com>
2020-03-30 16:48:08 +0300

    Add delete function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+class ToDoList extends React.Component {
+  state = {
+    todos:[ 
+      { 
+       id: 1,
+       title: 'Go shoping',
+       taskComplete: 'false'
+      },
+      
+      { 
+        id: 2,
+        title: 'Spam viber',
+        taskComplete: 'false'
+      },
+      
+      { 
+        id: 3,
+        title: 'Take out the trashhh',
+        taskComplete: 'false'
+      }
+    ]
+  }
+
+  handleDeleteOnClick(event, todoId) {
+    this.setState({todos: this.state.todos.filter( todo => todo.id !== todoId)})
+  }
+
+  render() {
+    return (
+      <div className="to-do-container"> 
+        {this.state.todos.map( todo => <div className="to-do-container" key={todo.id}> 
+            {todo.title}
+            <button onClick={(event) => this.handleDeleteOnClick(event, todo.id)}>Delete</button> 
+          </div> ) }
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ToDoList />,
   document.getElementById('root')
 );
 
